03b84df92f5522db449d701f9710b7efeed11953
gamp300 <gamp300@gmail.com>
2024-02-20 15:41:57 -0500

    Revert "React 04-Feedback"
    This reverts commit bcaf20efe453298b3beea88e2c971cb200c81436.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,16 @@
-import React, { useState } from 'react';
-import Statistics from './Statistics';
-import FeedbackOptions from './FeedbackOptions';
-import Section from './Section';
-import Notification from './Notification';
-
 export const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  const handleFeedback = type => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [type]: prevFeedback[type] + 1,
-    }));
-  };
-
-  const countTotalFeedback = () => {
-    return feedback.good + feedback.neutral + feedback.bad;
-  };
-
-  const countPositiveFeedbackPercentage = () => {
-    const total = countTotalFeedback();
-    return total === 0 ? 0 : Math.round((feedback.good / total) * 100);
-  };
-
-  const totalFeedback = countTotalFeedback();
-  const positivePercentage = countPositiveFeedbackPercentage();
-
   return (
-    <div>
-      <Section title="Expresso Café Feedback">
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={handleFeedback}
-        />
-      </Section>
-
-      <Section title="Statistics">
-        {totalFeedback === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
-          <Statistics
-            good={feedback.good}
-            neutral={feedback.neutral}
-            bad={feedback.bad}
-            total={totalFeedback}
-            positivePercentage={positivePercentage}
-          />
-        )}
-      </Section>
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
     </div>
   );
 };
